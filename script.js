e1f544c1858a4743beaaa7bc9c6c175744f5a76a
Mayank Kashyap <107757811+Memomer@users.noreply.github.com>
2023-01-16 22:03:56 +0530

    html, css and js files uploaded

---
+++ script.js
@@ -0,0 +1,41 @@
+const { update_await_block_branch, text } = require("svelte/internal");
+
+const reviews = [
+    {
+        id: 1,
+        name: "susan smith",
+        job: "web developer",
+        img: 
+          "https://res.cloudinary.com/diqq",
+        text:
+           "I'm baby meggings twee health goth +1. Bicycle rights
+             tumeric chartreuse before they sold out chambray pop-up
+             Shaman humblebrag pickled coloring book salvia hoodie,
+             cold-pressed four dollar toast everyday carry",
+    },
+    
+    {
+        id: 2,
+        name: "anna johnson",
+        job: "web designer",
+        img:
+           lor
+        text:
+            ,
+    },
+    {
+        id:2,
+        name:"anna johnson",
+        job:"web designer",
+        img:
+          "h"
+        text:
+            ""
+    },
+    {
+        id:
+        name:
+        job:
+        img:
+    }
+]
